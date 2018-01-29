5cacddca8361caa1d1872314bbff53e7fab057da
Imputes <zhuzi@Bobbys-MacBook-Air.local>
2018-01-29 18:09:39 +0800

    Reusing custom xib

--- Nike Collection/controllers/Detail Tab/OrderSummary/PaymentTableViewCell.swift
+++ Nike Collection/controllers/Detail Tab/OrderSummary/PaymentTableViewCell.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class PaymentTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet weak var cardNumberLaber: UILabel!
+    @IBOutlet weak var nameOnCardLabel: UILabel!
+    @IBOutlet weak var expirationLabel: UILabel!
+    
+    let shoppingCart = ShoppingCart.sharedInstance
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -22,3 +29,21 @@ class PaymentTableViewCell: UITableViewCell {
     }
 
 }
+
+//custom funcitons
+extension PaymentTableViewCell{
+    func configueCell()  {
+        if let creditCard = shoppingCart.creditCard{
+            let cardType = creditCard.type
+            cardImageView.image = UIImage.init(named: cardType!)
+            cardNumberLaber.text = creditCard.cardNumber?.maskedPlusLast4()
+            nameOnCardLabel.text = creditCard.nameOnCard
+            expirationLabel.text = "\(creditCard.expMonth)/\(creditCard.expYear)"
+        
+        
+        }
+    }
+}
+
+
+
--- Nike Collection/controllers/Detail Tab/OrderSummary/ShippingTableViewCell.swift
+++ Nike Collection/controllers/Detail Tab/OrderSummary/ShippingTableViewCell.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ShippingTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var customNameLabel: UILabel!
+    @IBOutlet weak var addAddress1Label: UILabel!
+    @IBOutlet weak var address2Label: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var zipLabel: UILabel!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+
+    let shoppingCart = ShoppingCart.sharedInstance
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -21,4 +31,26 @@ class ShippingTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+//custom functions
+extension ShippingTableViewCell{
+    
+    func configureCell(){
+        if let customr = shoppingCart.customer, let shippingAddress = shoppingCart.shippingAddress{
+          customNameLabel.text = customr.name
+            phoneLabel.text = customr.phone
+            addAddress1Label.text = shippingAddress.address1
+            if let address2 = shippingAddress.address2{
+            address2Label.text = address2
+            }else{
+            address2Label.text = ""
+            }
+            cityLabel.text = "\(shippingAddress.city!),"
+            stateLabel.text = shippingAddress.state
+            zipLabel.text = shippingAddress.zip
+            
+        }
+    }
+    
 }
