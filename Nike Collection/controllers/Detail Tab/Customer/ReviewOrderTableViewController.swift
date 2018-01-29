246c62024b5b04701db08d30855a3da2933a918a
Imputes <zhuzi@Bobbys-MacBook-Air.local>
2018-01-29 15:32:49 +0800

    Review order

---
+++ Nike Collection/controllers/Detail Tab/Customer/ReviewOrderTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ReviewOrderTableViewController.swift
+//  Nike Collection
+//
+//  Created by Bobby Negoat on 1/29/18.
+//  Copyright © 2018 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ReviewOrderTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+   
+
+}
---
+++ Nike Collection/controllers/Detail Tab/OrderSummary/ShippingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShippingTableViewCell.swift
+//  Nike Collection
+//
+//  Created by Bobby Negoat on 1/29/18.
+//  Copyright © 2018 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ShippingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
